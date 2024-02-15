b07d01be3f3645eb47e80e312b8b050709e9c8c4
YoojinLee <yoojinlee23800@gmail.com>
2024-02-15 10:38:08 +0900

    Add todos project to Git

---
+++ index.js
@@ -0,0 +1,5 @@
+const textarea = document.querySelector('textarea')
+const addBtn = document.getElementById('addBtn')
+const todoContainer = document.querySelector('.todoContainer')
+
+console.log(todoContainer.innerHTML)
